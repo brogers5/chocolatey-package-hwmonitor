5b0ec2b635a02e84ddef1c0df74d902569e4529b
ionred <ionred@users.noreply.github.com>
2022-11-08 18:17:36 -0600

    AU: 1 updated - hwmonitor [skip ci] https://gist.github.com/fd371814a0847427a5a07a141abeec9a/b7e858cc2d8b32ad5d794fb4c9eff7a3fecb2d3d

--- automatic/hwmonitor/tools/chocolateyInstall.ps1
+++ automatic/hwmonitor/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $packageName = 'hwmonitor';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)";
 $installerType = 'EXE';
-$url = 'http://download.cpuid.com/hwmonitor/hwmonitor_1.46.exe';
+$url = 'https://download.cpuid.com/hwmonitor/hwmonitor_1.47.exe';
 
 $packageArgs = @{
   packageName   = $packageName
@@ -13,7 +13,7 @@ $packageArgs = @{
   silentArgs    = '/SILENT /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
   softwareName  = '*hwmonitor*'
-  checksum      = '151bec617b393c99ddb59b3b7fb4fd150ebdae2222e89f25151252b231795e0a'
+  checksum      = '78d3d4cd218b5c9cf3bdba1021a6b4e93ec1d6a5afbe260bb8900221b5e3c4a6'
   checksumType  = 'sha256'
 };
 Install-ChocolateyPackage @packageArgs;
